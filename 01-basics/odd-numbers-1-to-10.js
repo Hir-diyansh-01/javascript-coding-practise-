27802f853db02532570dd20cfd447300864dfe4f
Hir-diyansh-01 <hirdiyanshmishra@gmail.com>
2026-01-27 13:49:03 +0530

    Added odd numbers from 1 to 10

---
+++ 01-basics/odd-numbers-1-to-10.js
@@ -0,0 +1,15 @@
+/*
+Question:
+Print odd numbers from 1 to 10
+
+Logic:
+- Loop from 1 to 10
+- Check if number is NOT divisible by 2
+- If yes, print it
+*/
+
+for (let i = 1; i <= 10; i++ ){
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
